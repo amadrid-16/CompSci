575b27ef648be108ae678bbaacce8bb5531af716
Angel Madrid <amadrid_16@esusdstudents.org>
2015-02-16 00:36:46 -0800

    Revert "shaq"
    This reverts commit 6da7f6b4531b8b6f7d71376224dc5fcbc8a9247d.

--- newrepoz.java
+++ newrepoz.java
@@ -2,7 +2,6 @@ public class newrepoz{
    public static void main(String[] args){
       System.out.println("This is great if this works");
       now that this is not a shaq lord
-      mother shaqqqqereeerrrrrrrr
       
    }
 }
