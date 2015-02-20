7603aca88544a55cca55389fdc3510ef5c0ddf25
Angel Madrid <amadrid_16@esusdstudents.org>
2015-02-20 09:31:37 -0800

    worked on Checkout worked with arraylistssss

--- Testing/Checkout.java
+++ Testing/Checkout.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
 public class Checkout{
-   candy
-   ice cream
-   sundae
-   cookie
+
+   public int numberOfItems;
+   ArrayList<DessertItem> checkoutList;//must be accessable to all the methods
    public Checkout(){
+      //ArrayList<variable type> ListName = new ArrayList<variable type>(initial size);
+   
+      //also import java.util.ArrayList
+   
+      //name.add(object);
+     checkoutList = new ArrayList<DessertItem>(1);//manipulates the given array list
       
    }
-   public clear(){
-   
+   public int numberOfItems(){
+      Object[] checkoutArray = checkoutList.toArray();
+      checkoutArray = checkoutList.toArray(checkoutArray);
+      return numberOfItems;
    }
 }
