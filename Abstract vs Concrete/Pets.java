b6e7935b0c8d263be266e6c1a83d634fa439da50
Angel Madrid <amadrid_16@esusdstudents.org>
2015-03-01 00:05:28 -0800

    deleted files
    not sure

--- Abstract vs Concrete/Pets.java
+++
@@ -1,7 +0,0 @@
-public interface Pets{//interface only methods that contain nothing cannot have variables
-
-   public abstract void beAdorable();
-   
-   public abstract void play();
-
-}
--- Rectangle/RectangleMaker.java
+++
@@ -1,9 +0,0 @@
-public class RectangleMaker {
-            
-    public static void main(String []args){
-       Rectangle Perim = new Rectangle(5,5);
-       
-       System.out.println(Perim.width);
-    
-    }
-}
